b78b327616a404389314ea3d5d9edd831d3fc618
Georg-JohannSeeder <115535288+Georg-JohannSeeder@users.noreply.github.com>
2023-02-21 12:05:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,42 @@
 import pygame
-import sys
-pygame.init()
 
-#värvid
-lGreen = [153, 255, 153]
-lBlue = [153, 204, 255]
-
-#ekraani seaded
-screen=pygame.display.set_mode([640,480])
+screen = pygame.display.set_mode([640, 480])
 pygame.display.set_caption("Harjutamine")
-screen.fill(lGreen)
 
-gameover = False
 
-while not gameover:
+GREEN = [153, 255, 153]
+RED = [255, 0, 0]
 
-    #Lisame pildid
-    youWin = pygame.image.load("img/youwin.png")
-    youWin = pygame.transform.scale(youWin, [300, 120])
-    screen.blit(youWin,[180,100])
 
-    pygame.display.flip()
 
-    # mängu sulgemine ristist
-    for i in pygame.event.get():
-        if i.type == pygame.QUIT:
-            sys.exit()
+class ruudud:
+    def __init__(self, color, sizea, sizeb, colorbg):
+        self.color = color
+        self.sizea = sizea
+        self.sizeb = sizeb
+        self.colorbg = colorbg
 
+    def ruudustik (self):
+        f = 1
+        screen.fill(self.colorbg)
+        for i in range(0, 50, 1):
+            e = 1
+            for j in range(0, 50, 1):
+                pygame.draw.rect(screen, self.color, [e, f, self.sizea, self.sizeb])
+                e += 18
+            f += 18
+
+
+ruudud.ruudustik (ruudud(GREEN, 15, 15, RED))
+
+pygame.display.flip()
+
+running = True
+while running:
+    for event in pygame.event.get():
+
+        if event.type == pygame.QUIT:
+            running = False
+
+    pygame.display.update()
 pygame.quit()
